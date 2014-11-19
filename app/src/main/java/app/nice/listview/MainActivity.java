3c44aaef5000f76c32bf22fd8ab0ef1694246aef
Ethan Woo <ethan.woo@gmail.com>
2014-11-19 13:55:25 -0500

    Initial commit

---
+++ app/src/main/java/app/nice/listview/MainActivity.java
@@ -0,0 +1,70 @@
+package app.nice.listview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.text.FieldPosition;
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] favoriteTVShows = {"nice","Ding Ding Dong","Infinite Challenge","Gotham","Iron Man",
+                "Superman","Interstellar"};
+
+        ListAdapter theAdapter = new MyAdapter(this, favoriteTVShows);
+
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new
+                AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                    String tvShowPicked = "You selected " +
+                            String.valueOf(adapterView.getItemAtPosition(position));
+
+                    Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show();
+                }
+             });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
